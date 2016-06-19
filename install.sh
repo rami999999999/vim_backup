d1a493f4f4cf44854680ae7df44f96b131eeb705
Pedro Ramos <pedrolorgaramos@gmail.com>
2016-06-19 15:22:19 +0100

    zsh stuff

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 ln -s /usr/share/vim/vimfiles /usr/share/nvim/runtime
 
-
+cp ./zsh/agnoster.custom* /usr/share/oh-my-zsh/themes
 
 
 echo "install xsel!!!!"
